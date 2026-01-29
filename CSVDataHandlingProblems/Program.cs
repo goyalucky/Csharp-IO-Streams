7c2697ab92cdbd3a699d489fb783f58e6278667c
goyallucky <luckygoyal221982@gmail.com>
2026-01-29 12:57:22 +0530

    [Lucky] Add Program.cs as entry point to run all csv programs

---
+++ CSVDataHandlingProblems/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CSVDataHandlingProblems;
+using System;
+using CSVStudentApp;
+using CSVEmployeeApp;
+using CSVCountApp;
+using RowFilterApp;
+using SearchApp;
+using UpdateApp;
+using SortApp;
+using ValidationApp;
+using DataToObjects;
+using MergingFiles;
+using LargeCSVReader;
+using DetectDuplicates;
+using JSONtoCSVConvertor;
+using EncryptDecrypt;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        JsonMain.Execute();
+        // EncryptDecrypt.EmployeeMain.Execute();
+        // DuplicateMain.Execute();
+        // CSVReaderMain.Execute();
+        // MergeMain.Execute();
+        // StudentsMain.Execute();
+        // ValidationMain.Execute();
+        // SortMain.Execute();
+        // UpdateMain.Execute();
+        // SearchMain.Execute();
+        // FilterRowMain.Execute();
+        // CountMain.Execute();
+        // EmployeeMain.Start();
+        // StudentMain.Start();
+    }
+}
+
