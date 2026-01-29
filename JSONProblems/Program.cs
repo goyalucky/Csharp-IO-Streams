a922287a5e989da9612094b18d650e83e8d2cd6e
goyallucky <luckygoyal221982@gmail.com>
2026-01-29 14:21:00 +0530

    [Lucky] Add main entry point to execute JSON and IPL handlers

---
+++ JSONProblems/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace JSONProblems;
+using IPLDataCensorship;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // StudentJson.Execute();
+        // CarToJson.Execute();
+        // ReadSpecificFields.Execute();
+        // SimpleValidateJson.Execute();
+        // FilterJsonByAge.Execute();
+        // ListToJson.Execute();
+        // MergeJsonObjects.Execute();
+        ReadJsonPrintKeysValues.Execute();
+
+
+
+        // IPLMain.Execute();
+    }
+}
